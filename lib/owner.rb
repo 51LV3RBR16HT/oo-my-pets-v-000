9bc7bb5c5f807bb6618ba51d9b186d36fd356f34
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-21 21:59:10 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,12 +3,13 @@ require 'pry'
 class Owner
 
   attr_accessor :name
-  attr_reader :species
+  attr_reader :species, :owner
 
   @pets = {fishes: [], cats: [], dogs: []}
 
   def initialize(name)
     @name = name
+    @
   end
 
 
