9b451cc5cca9af5b8dff673db467dd192857de49
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-21 23:00:17 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -5,9 +5,9 @@ class Cat
   attr_accessor :mood
   attr_reader :name, :species
 
-  def initialize(name, mood)
+  def initialize(name, mood = "nervous")
     @name = name
-    @mood = "nervous"
+    @mood = mood
   end
 
 end
