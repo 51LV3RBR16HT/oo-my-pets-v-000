60ce000df195c00dcf3b221cbc7c963674384a1b
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-21 21:29:05 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -5,13 +5,9 @@ class Cat
   attr_accessor :mood
   attr_reader :name
 
-  def initialize(name, mood)
+  def initialize(name)
     binding.pry
     @name = name
-    @mood = mood
-
-
-
   end
 
 end
