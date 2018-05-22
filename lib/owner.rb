9a8d044507eb6cd73213da24aa9014b00f0cf1b9
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-22 00:15:13 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -62,7 +62,7 @@ class Owner
 
   def sell_pets
     pets.each do |type, pets|
-        pets.map {|pet| pet.mood = "nervous"}
+        pets.each {|pet| pet.mood = "nervous"}
       end
     pets.clear
   end
